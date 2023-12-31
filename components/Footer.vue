d09a78b2902b15b91818c3d8b20faa82ee4c5801
bintang <155158627+binscy@users.noreply.github.com>
2023-12-31 11:50:15 +0700

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
 <footer>
   <div class="container mx-auto ">
-    <p class="text-center text-sm my-0 py-5">2020-PRESENT © Ahmad Saugi</p>
+    <p class="text-center text-sm my-0 py-5">2021-PRESENT © Radian Bintang</p>
   </div>
 </footer>
 </template>
